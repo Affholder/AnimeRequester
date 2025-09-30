4c3993a1d9622c1feede3ba57fb8dc3a7e9c726b
Quentin <quentin@affholder.fr>
2025-09-30 15:17:32 +0200

    add: add button for the API key

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,29 @@
-const apikey = prompt("Veuillez entrer votre clé API: ");
-if (apikey){
-  alert("Clé API enregistrée !");
-} 
-else {
-  alert("Clé API obligatoire !");
+APIKEY = null;
+function setApiKey() {
+  APIKEY = prompt("Veuillez entrer votre clé API: ");
+  const ulElement = document.querySelector("ul");
+
+  fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=20", {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": APIKEY.trim(),
+      "X-RapidAPI-Host": "anime-db.p.rapidapi.com"
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      for (const anime of data.data) {
+        const liElement = document.createElement("li");
+        // genres est un tableau → on le joint avec des virgules
+        liElement.innerText = `${anime.title} → ${anime.genres.join(", ")}`;
+        ulElement.appendChild(liElement);
+      }
+    })
+    .catch(err => console.error(err));
+  if (APIKEY) {
+    alert("Clé API enregistrée !");
+  }
+  else {
+    alert("Clé API obligatoire !");
+  }
 }
-
-const ulElement = document.querySelector("ul");
-
-fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10", {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": apikey.trim(),
-    "X-RapidAPI-Host": "anime-db.p.rapidapi.com"
-  }
-})
-.then(response => response.json())
-.then(data => {
-  for (const anime of data.data) {
-    const liElement = document.createElement("li");
-    // genres est un tableau → on le joint avec des virgules
-    liElement.innerText = `${anime.title} → ${anime.genres.join(", ")}`;
-    ulElement.appendChild(liElement);
-  }
-})
-.catch(err => console.error(err));
-
