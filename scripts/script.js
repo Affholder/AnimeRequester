62b49fb9e7811619e47d50f68a6ff059632e6480
Quentin <quentin@affholder.fr>
2025-09-30 15:04:57 +0200

    add: implement prompt for API key and fetch anime data

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,28 @@
-// Fonction pour gérer l'API
-async function searchAnime(query) {
-    const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '7168f98cfdmsh3eaffd2b287f39fp16be4bjsn5224cfc56766',
-        'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
-      }
-    };
-    
-    const response = await fetch(url, options);
-    return await response.json();
+const apikey = prompt("Veuillez entrer votre clé API: ");
+if (apikey){
+  alert("Clé API enregistrée !");
+} 
+else {
+  alert("Clé API obligatoire !");
+}
+
+const ulElement = document.querySelector("ul");
+
+fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10", {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": apikey.trim(),
+    "X-RapidAPI-Host": "anime-db.p.rapidapi.com"
   }
+})
+.then(response => response.json())
+.then(data => {
+  for (const anime of data.data) {
+    const liElement = document.createElement("li");
+    // genres est un tableau → on le joint avec des virgules
+    liElement.innerText = `${anime.title} → ${anime.genres.join(", ")}`;
+    ulElement.appendChild(liElement);
+  }
+})
+.catch(err => console.error(err));
+
