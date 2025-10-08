682609485121d472f32853683bcd2e333d953b65
Quentin <quentin@affholder.fr>
2025-10-08 13:58:28 +0200

    add: display anime and research by name

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,27 @@
 APIKEY = null;
+
+// Fonction utilisée pour saisir la clé API et l'enregistrer
 function setApiKey() {
   APIKEY = prompt("Veuillez entrer votre clé API: ");
-  const ulElement = document.querySelector("ul");
+  if (APIKEY) {
+    alert("Clé API enregistrée !");
+  }
+  else {
+    alert("Clé API obligatoire !");
+  }
+}
 
-  fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=20", {
+// Fonction utilisée pour recherher un anime
+function rechercheAnime() {
+  if (!APIKEY || APIKEY.trim() === "") {
+    alert("Veuillez d'abord entrer votre clé API !");
+    return;
+  }
+  let nom = document.getElementById("champRecherche").value;
+  console.log(nom);
+  const ulElement = document.querySelector("ul");
+  ulElement.innerHTML = "";
+  fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search="+nom.trim(), {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": APIKEY.trim(),
@@ -14,16 +32,19 @@ function setApiKey() {
     .then(data => {
       for (const anime of data.data) {
         const liElement = document.createElement("li");
-        // genres est un tableau → on le joint avec des virgules
-        liElement.innerText = `${anime.title} → ${anime.genres.join(", ")}`;
+        const h1Element = document.createElement("h1");
+        const imgElement = document.createElement("img");
+        h1Element.innerText = `${anime.title}`;
+        imgElement.src = `${anime.image}`;
+        liElement.innerText = `${anime.synopsis} \n ${anime.genres.join(", ")} \n rank :${anime.ranking}\n Episodes : ${anime.episodes}` ;
+        ulElement.appendChild(h1Element);
+        ulElement.appendChild(imgElement);
         ulElement.appendChild(liElement);
       }
     })
     .catch(err => console.error(err));
-  if (APIKEY) {
-    alert("Clé API enregistrée !");
-  }
-  else {
-    alert("Clé API obligatoire !");
-  }
+}
+
+function afficherAnime(data){
+
 }
