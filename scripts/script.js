959534a98482c0c4e8d72debd475bf01c05cf8a4
Quentin <quentin@affholder.fr>
2025-10-08 14:56:27 +0200

    add: packed anime in div + id for css

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,13 @@ function setApiKey() {
   }
 }
 
+document.getElementById("champRecherche").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    rechercheAnime();
+  }
+});
+
 // Fonction utilisée pour recherher un anime
 function rechercheAnime() {
   if (!APIKEY || APIKEY.trim() === "") {
@@ -21,7 +28,7 @@ function rechercheAnime() {
   console.log(nom);
   const ulElement = document.querySelector("ul");
   ulElement.innerHTML = "";
-  fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search="+nom.trim(), {
+  fetch("https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=" + nom.trim(), {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": APIKEY.trim(),
@@ -30,21 +37,29 @@ function rechercheAnime() {
   })
     .then(response => response.json())
     .then(data => {
-      for (const anime of data.data) {
-        const liElement = document.createElement("li");
-        const h1Element = document.createElement("h1");
-        const imgElement = document.createElement("img");
-        h1Element.innerText = `${anime.title}`;
-        imgElement.src = `${anime.image}`;
-        liElement.innerText = `${anime.synopsis} \n ${anime.genres.join(", ")} \n rank :${anime.ranking}\n Episodes : ${anime.episodes}` ;
-        ulElement.appendChild(h1Element);
-        ulElement.appendChild(imgElement);
-        ulElement.appendChild(liElement);
-      }
+      afficherAnime(data, ulElement);
     })
     .catch(err => console.error(err));
 }
 
-function afficherAnime(data){
-
+function afficherAnime(data, ulElement) {
+  for (const anime of data.data) {
+    const divCard = document.createElement("div");
+    divCard.id = "divCard";
+    const pSynopsisElement = document.createElement("p");
+    pSynopsisElement.id = "pSynopsisElement";
+    const pInfoElement = document.createElement("p");
+    pInfoElement.id = "pInfoElement";
+    const h1Element = document.createElement("h1");
+    const imgElement = document.createElement("img");
+    h1Element.innerText = `${anime.title}`;
+    imgElement.src = `${anime.image}`;
+    pSynopsisElement.innerText = `${anime.synopsis}`;
+    pInfoElement.innerText = `${anime.genres.join(", ")} \n rank :${anime.ranking}\n Episodes : ${anime.episodes}`;
+    divCard.appendChild(h1Element);
+    divCard.appendChild(imgElement);
+    divCard.appendChild(pSynopsisElement);
+    divCard.appendChild(pInfoElement);
+    ulElement.appendChild(divCard);
+  }
 }
